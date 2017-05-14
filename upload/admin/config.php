d48c0b71467cb7699c9c01c0dfa6b919039e3df8
erikl2412 <erikluu11111@gmail.com>
2017-05-14 15:26:42 +0700

    config

---
+++ upload/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart/upload/admin/');
+define('HTTP_CATALOG', 'http://localhost/opencart/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart/upload/admin/');
+define('HTTPS_CATALOG', 'http://localhost/opencart/upload/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/wamp64/www/opencart/upload/admin/');
+define('DIR_SYSTEM', 'C:/wamp64/www/opencart/upload/system/');
+define('DIR_IMAGE', 'C:/wamp64/www/opencart/upload/image/');
+define('DIR_LANGUAGE', 'C:/wamp64/www/opencart/upload/admin/language/');
+define('DIR_TEMPLATE', 'C:/wamp64/www/opencart/upload/admin/view/template/');
+define('DIR_CONFIG', 'C:/wamp64/www/opencart/upload/system/config/');
+define('DIR_CACHE', 'C:/wamp64/www/opencart/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/wamp64/www/opencart/upload/system/storage/download/');
+define('DIR_LOGS', 'C:/wamp64/www/opencart/upload/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/wamp64/www/opencart/upload/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/wamp64/www/opencart/upload/system/storage/upload/');
+define('DIR_CATALOG', 'C:/wamp64/www/opencart/upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ upload/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart/upload/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/wamp64/www/opencart/upload/catalog/');
+define('DIR_SYSTEM', 'C:/wamp64/www/opencart/upload/system/');
+define('DIR_IMAGE', 'C:/wamp64/www/opencart/upload/image/');
+define('DIR_LANGUAGE', 'C:/wamp64/www/opencart/upload/catalog/language/');
+define('DIR_TEMPLATE', 'C:/wamp64/www/opencart/upload/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/wamp64/www/opencart/upload/system/config/');
+define('DIR_CACHE', 'C:/wamp64/www/opencart/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/wamp64/www/opencart/upload/system/storage/download/');
+define('DIR_LOGS', 'C:/wamp64/www/opencart/upload/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/wamp64/www/opencart/upload/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/wamp64/www/opencart/upload/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
